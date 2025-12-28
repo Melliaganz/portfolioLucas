31a06678ed52477fc0493dbc895a703957a4c02b
Melliaganz <lucaslengranddev@gmail.com>
2025-12-28 22:16:41 +0100

    image compressé

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 
 export default defineConfig({
   plugins: [
     react(),
+    cssInjectedByJsPlugin({
+      topExecutionPriority: true,
+    }),
   ],
-})
+  build: {
+    modulePreload: {
+      polyfill: false,
+    },
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+});
