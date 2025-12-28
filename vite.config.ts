2839ee531d14e2935e38c80058bd837be78ea923
Melliaganz <lucaslengranddev@gmail.com>
2025-12-28 22:45:12 +0100

    index.html

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,12 @@ export default defineConfig({
   plugins: [react()],
   build: {
     cssCodeSplit: false, 
-    assetsInlineLimit: 4096, 
+    assetsInlineLimit: 4096,
     rollupOptions: {
       output: {
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
+        assetFileNames: 'assets/[ext]/[name]-[hash].[ext]',
         manualChunks(id) {
           if (id.includes('node_modules')) {
             return 'vendor';
