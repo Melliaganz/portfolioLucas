ae29eba948e998511902eba5958f9068afcce857
Melliaganz <lucaslengranddev@gmail.com>
2025-12-28 22:51:18 +0100

    index.html

--- vite.config.ts
+++ vite.config.ts
@@ -4,17 +4,12 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
+    assetsInlineLimit: 10000, 
     cssCodeSplit: false, 
-    assetsInlineLimit: 4096,
     rollupOptions: {
       output: {
-        chunkFileNames: 'assets/js/[name]-[hash].js',
-        entryFileNames: 'assets/js/[name]-[hash].js',
-        assetFileNames: 'assets/[ext]/[name]-[hash].[ext]',
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return 'vendor';
-          }
+        manualChunks: {
+          'vendor': ['react', 'react-dom'],
         },
       },
     },
