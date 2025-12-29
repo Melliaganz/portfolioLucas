4291c8adee9aa5e30f326d57b0afe289690c4b0a
Melliaganz <lucaslengranddev@gmail.com>
2025-12-29 01:03:57 +0100

    chargement image tot

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,25 @@
+import { Suspense, lazy } from "react";
 import "./App.css";
 import { Header } from "./components/Header";
-import { Footer } from "./components/Footer";
-
 import { Hero } from "./page/Hero";
-import { Parcours } from "./page/Parcours";
-import { Projects } from "./page/Projects";
-import { Contact } from "./page/Contact";
+
+const Parcours = lazy(() => import("./page/Parcours").then(module => ({ default: module.Parcours })));
+const Projects = lazy(() => import("./page/Projects").then(module => ({ default: module.Projects })));
+const Contact = lazy(() => import("./page/Contact").then(module => ({ default: module.Contact })));
+const Footer = lazy(() => import("./components/Footer").then(module => ({ default: module.Footer })));
 
 function App() {
   return (
     <main>
       <Header />
-      <Hero />
-      <Parcours />
-      <Projects />
-      <Contact />
-      <Footer />
+      <Hero /> 
+      
+      <Suspense fallback={null}>
+        <Parcours />
+        <Projects />
+        <Contact />
+        <Footer />
+      </Suspense>
     </main>
   );
 }
