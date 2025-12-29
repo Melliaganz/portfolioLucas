e3cb51f89a78348b6e5ffc0736fc4a76a08c2a43
Melliaganz <lucaslengranddev@gmail.com>
2025-12-29 01:10:42 +0100

    mince

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,14 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    reportCompressedSize: true,
-    chunkSizeWarningLimit: 500,
-  },
-});
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    }
+  }
+})
