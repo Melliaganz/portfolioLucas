370f2345c68b9e8d15cb6acbac02b8ce13b87c23
Melliaganz <lucaslengranddev@gmail.com>
2026-01-15 12:30:59 +0100

    upgrade package

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, mergeConfig } from "vite";
+import { defineConfig, mergeConfig, type UserConfig } from "vite";
 import { defineConfig as defineVitestConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
@@ -40,8 +40,6 @@ const viteConfig = defineConfig({
     cssMinify: 'esbuild', 
     rollupOptions: {
       output: {
-        // En mode PnP, les chemins ne contiennent plus "node_modules". 
-        // Si vous utilisez vite-plugin-singlefile, manualChunks n'est de toute façon pas utilisé.
         manualChunks: undefined 
       }
     },
@@ -55,14 +53,13 @@ const vitestConfig = defineVitestConfig({
     environment: 'jsdom',
     setupFiles: './src/test/setup.ts',
     include: ['src/**/*.{test,spec}.{ts,tsx}'],
-    // Correction pour Yarn PnP sur Linux/GitHub Actions :
-    pool: 'threads',
+    pool: 'forks',
     poolOptions: {
-      threads: {
-        singleThread: true,
+      forks: {
+        singleFork: true,
       },
-    },
-  },
+    }
+  } as any,
 });
 
-export default mergeConfig(viteConfig, vitestConfig);
+export default mergeConfig(viteConfig as UserConfig, vitestConfig as UserConfig);
